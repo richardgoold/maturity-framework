843daa27ce0fa7b66d1986e10d1575f91c592964
richardgoold <richard@richardgoold.com>
2026-02-24 13:30:18 +0000

    Fix navbar logo onClick syntax error

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -30,7 +30,7 @@ function NavBar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="cursor-pointer" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' }>
+          <div className="cursor-pointer" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
             <img src="/GrowthLens%20Logo_no%20strapline.png" alt="GrowthLens" className="h-8 w-auto" />
           </div>
 
